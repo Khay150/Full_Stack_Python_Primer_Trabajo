046d0c7f4c8bcacc9092f2485bf8c15b60d16c13
Khay150 <fvanotti15@gmail.com>
2024-05-23 13:43:20 -0300

    Adicion del formulario de consultas en FAQ

---
+++ validacion_formulario.js
@@ -0,0 +1,71 @@
+
+(function(){
+
+    var formulario = document.getElementsByName('formulario')[0];
+
+    var validarNombre = function(e){
+        if (formulario.nombre.value == 0){
+            alert("Completa el campo nombre");
+            e.preventDefault();
+            }
+            else{
+                if (formulario.nombre.value.length > 20){
+                    alert("Nombre extenso");
+                    e.preventDefault();
+                    }
+            }
+        };
+
+    var validarApellido = function(e){
+        if (formulario.apellido.value == 0){
+            alert("Completa el campo apellido");
+            e.preventDefault();
+            }
+            else{
+                if (formulario.nombre.value.length > 20){
+                    alert("apellido extenso");
+                    e.preventDefault();
+                    }
+            }
+        };    
+
+    var validarRadio = function(e){
+        if (formulario.sexo[0].checked == true ||
+            formulario.sexo[1].checked == true || formulario.sexo[2].checked == true){
+        }
+        else{
+            alert("Completa el campo sexo");
+            e.preventDefault();
+            }
+        };
+
+    var validarCheckbox = function(e){
+        var chequeado = false;
+        for (var i = 0; i < 5; i++) {
+            if (formulario.asunto[i].checked == true){
+                chequeado = true
+            }
+        }
+        if (chequeado == false) {
+            alert("Seleccione su asunto");
+            e.preventDefault();
+            }
+        };
+
+    var validarTextbox = function(e){
+        var texto = formulario.mensaje.value;
+        if (texto.trim() == ""){
+            alert("Agregue su mensaje");
+            e.preventDefault();
+            }
+        };
+
+    var validar = function(e){
+        validarNombre(e);
+        validarApellido(e);
+        validarRadio(e);
+        validarCheckbox(e);
+        validarTextbox(e);
+    };
+    formulario.addEventListener("submit", validar);
+}())
