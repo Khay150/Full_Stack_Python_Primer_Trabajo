41d706b0d1801acadf16c4a08dd57d779e53533a
Emiliano-Torres <emilianomtorres1@gmail.com>
2024-06-28 19:07:15 -0300

    formateo de proyecto 2 añadido de rutas nuevas

--- crud.py
+++ crud.py
@@ -4,12 +4,25 @@ from flask import render_template
 # Creamos la aplicación
 app = Flask(__name__)
 # Proporcionamos la ruta a la raiz del sitio
-@app.route('/')
+@app.route('/index.html')
 def index():
     return render_template(
         'index.html')
 
+@app.route('/Catalogo.html')
+def Catalogo():
+    return render_template(
+        'Catalogo.html')
 
+
+@app.route('/nosotros.html')
+def Nosotros():
+    return render_template(
+        'nosotros.html')
+@app.route('/FQ.html')
+def FAQ():
+    return render_template(
+        'FQ.html')
 
 if (__name__=="__main__"):
     app.run(debug=True)
